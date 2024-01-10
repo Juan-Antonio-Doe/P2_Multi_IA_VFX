d5af14eff2a4c564d77b8de9722efba21f6c618e
El_Espectro <jaespinosag@esimurcia.com>
2024-01-10 17:40:14 +0100

    [Trap] POR FIN!!!!! Sincronizadas trampas.
    + Sincronizadas trampas sin los bugs:
    - Que activa también la colocación de trampas en el local del otro jugador.
    - Que se cree una duplicación de la trampa que se queda en su posición inicial incluso tras ser colocada la otra trampa. (Ya no se generen duplicados en ningún momento en el remoto).

--- Assets/Scripts/Game/Player/Attack/TrapPlacer.cs
+++ Assets/Scripts/Game/Player/Attack/TrapPlacer.cs
@@ -75,13 +75,14 @@ public class TrapPlacer : MonoBehaviour, IOnEventCallback {
             moneyCostText.text = tempTrap != null ? $"{tempTrap.name.Replace("(Clone)", "")} | - {tempTrap.MoneyCost}" : "";
 
         //Cambiamos al modo de colocar trampas
-        if (Input.GetKeyDown(enterTrapModeKey)) {
-            if (playerManager.photonView.IsMine)
+        if (playerManager.photonView.IsMine)
+            if (Input.GetKeyDown(enterTrapModeKey)) {
+            
                 TryPlaceTrap();
 
-            Debug.Log($"I am: {playerManager.photonView.Owner.NickName}");
-            debugText = $"I am: {playerManager.photonView.Owner.NickName}";
-        }
+                Debug.Log($"I am: {playerManager.photonView.Owner.NickName}");
+                //debugText = $"I am: {playerManager.photonView.Owner.NickName}";
+            }
 
         if(playerManager.photonView.IsMine) {
             //Cuando la trampa temporal no es null, significa que estamos intentando colocar una trampa
@@ -104,9 +105,9 @@ public class TrapPlacer : MonoBehaviour, IOnEventCallback {
                     if (Input.GetKeyDown(placeTrapKey)) //Usando la key de colocar trampa, ponemos la trampa en el suelo
                     {
                         // Here call event for place trap
-                        PlaceTrap(Vector3.zero);
                         PhotonNetwork.RaiseEvent(TRAP_SYNC_PLACE, tempTrap.transform.position, new RaiseEventOptions { Receivers = ReceiverGroup.Others },
-                            SendOptions.SendReliable);
+                            SendOptions.SendReliable);  //----------- Null Exception (tempTrap.transform.position). Why? I don't know.
+                        PlaceTrap(Vector3.zero);
                         tempTrap = null;
                     }
                 }
@@ -119,8 +120,8 @@ public class TrapPlacer : MonoBehaviour, IOnEventCallback {
         {
             //Si NO existe la trampla placeholder, la instancia para entrar al modo de colocar trampa y poder ver donde se colocara
             case true:
-                //InstantiateTrap(currentTrapIndex);
-                PhotonNetwork.RaiseEvent(TRAP_SYNC_INSTANTIATION, currentTrapIndex, new RaiseEventOptions { Receivers = ReceiverGroup.All }, 
+                InstantiateTrap(currentTrapIndex);
+                PhotonNetwork.RaiseEvent(TRAP_SYNC_INSTANTIATION, currentTrapIndex, new RaiseEventOptions { Receivers = ReceiverGroup.Others }, 
                     SendOptions.SendReliable);
                 break;
             //Si SI existe la trampla placeholder, la destruye para salir del modo colocar trampa
@@ -129,26 +130,6 @@ public class TrapPlacer : MonoBehaviour, IOnEventCallback {
                 PhotonNetwork.RaiseEvent(TRAP_SYNC_DESTRUCTION, null, new RaiseEventOptions { Receivers = ReceiverGroup.All },
                                SendOptions.SendReliable);
                 break;
-        }
-    }
-
-    void PlaceTrap(Vector3 placePos) {
-        //Marcamos la trampa como colocada si no lo estaba aun
-        if (tempTrap != null && tempTrap.IsPlaced == false) {
-            if (playerManager.photonView.IsMine) {
-                playerManager.ChangeMoney(-tempTrap.MoneyCost);
-                tempTrap.owner = playerManager;
-                tempTrap.Place();
-                tempTrap = null;
-            }
-        }
-        if (remoteTrap != null && !remoteTrap.IsPlaced) {
-            if (!playerManager.photonView.IsMine) {
-                remoteTrap.owner = playerManager;
-                remoteTrap.transform.position = placePos;
-                remoteTrap.Place();
-                remoteTrap = null;
-            }
         }
     }
 
@@ -185,7 +166,7 @@ public class TrapPlacer : MonoBehaviour, IOnEventCallback {
 
     //Calcula y devuelve la posicion del suelo en la que hay que mover la trampa para mostrar donde se colocaria
     Vector3 GetRoundedCenterGroundPos(float _yPos) {
-        Vector3 _roundedPos = Vector3.zero;
+        Vector3 _roundedPos = new Vector3(0, -50f, 0);  // -50f for hide the trap
         Ray _ray;
         if (playerManager.photonView.IsMine) {
             _ray = cam.ViewportPointToRay(new Vector3(.5f, .5f, cam.nearClipPlane));
@@ -221,25 +202,58 @@ public class TrapPlacer : MonoBehaviour, IOnEventCallback {
                 currentTrapIndex = trapPrefabs.Length - 1;
             }
         }
-        Destroy(tempTrap.gameObject);
-        PhotonNetwork.RaiseEvent(TRAP_SYNC_DESTRUCTION, null, new RaiseEventOptions { Receivers = ReceiverGroup.All },
-                       SendOptions.SendReliable);
+        //Destroy(tempTrap.gameObject);
+        /*PhotonNetwork.RaiseEvent(TRAP_SYNC_DESTRUCTION, null, new RaiseEventOptions { Receivers = ReceiverGroup.All },
+                       SendOptions.SendReliable);*/
 
-        //InstantiateTrap(currentTrapIndex);
-        PhotonNetwork.RaiseEvent(TRAP_SYNC_INSTANTIATION, currentTrapIndex, new RaiseEventOptions { Receivers = ReceiverGroup.All },
+        
+        InstantiateTrap(currentTrapIndex);
+        PhotonNetwork.RaiseEvent(TRAP_SYNC_INSTANTIATION, currentTrapIndex, new RaiseEventOptions { Receivers = ReceiverGroup.Others },
             SendOptions.SendReliable);
     }
 
     // ---------------- Multiplayer-Adapted Methods ----------------
-    private void InstantiateTrap(int _currentTrapIndex) {
+    private void InstantiateTrap(int _currentTrapIndex, bool remote=false) {
         //tempTrap = Instantiate(trapPrefabs[_currentTrapIndex], trapPrefabs[_currentTrapIndex].transform.position, Quaternion.identity)/*.GetComponent<Trap>()*/;
         
         Trap auxTrap = Instantiate(trapPrefabs[_currentTrapIndex], trapPrefabs[_currentTrapIndex].transform.position, Quaternion.identity);
-        if (playerManager.photonView.IsMine) {
+        if (!remote) {
+            if (tempTrap != null)
+                Destroy(tempTrap.gameObject);
             tempTrap = auxTrap;
+            tempTrap.gameObject.name = tempTrap.gameObject.name.Replace("(Clone)", "[Local]");
         }
         else {
+            //Debug.Log("Remote Trap");
+            if (remoteTrap != null)
+                Destroy(remoteTrap.gameObject);
             remoteTrap = auxTrap;
+            remoteTrap.transform.position = new Vector3(0, -50f, 0);
+            remoteTrap.gameObject.name = remoteTrap.gameObject.name.Replace("(Clone)", "[Remote]");
+        }
+    }
+
+    void PlaceTrap(Vector3 placePos) {
+        //Marcamos la trampa como colocada si no lo estaba aun
+        if (tempTrap != null && tempTrap.IsPlaced == false) {
+            if (playerManager.photonView.IsMine) {
+                if (placePos.sqrMagnitude == Vector3.zero.sqrMagnitude) {
+                    playerManager.ChangeMoney(-tempTrap.MoneyCost);
+                    tempTrap.owner = playerManager;
+                    tempTrap.Place();
+                    tempTrap = null;
+                }
+            }
+        }
+        if (remoteTrap != null && !remoteTrap.IsPlaced) {
+            if (!playerManager.photonView.IsMine) {
+                if (placePos.sqrMagnitude != Vector3.zero.sqrMagnitude) {
+                    remoteTrap.owner = playerManager;
+                    remoteTrap.transform.position = placePos;
+                    remoteTrap.Place();
+                    remoteTrap = null;
+                }
+            }
         }
     }
 
@@ -255,8 +269,8 @@ public class TrapPlacer : MonoBehaviour, IOnEventCallback {
 
     public void OnEvent(EventData photonEvent) {
         if (photonEvent.Code == TRAP_SYNC_INSTANTIATION) {
-            //if (!playerManager.photonView.IsMine)
-                InstantiateTrap((int)photonEvent.CustomData);
+            if (!playerManager.photonView.IsMine)
+                InstantiateTrap((int)photonEvent.CustomData, true);
         }
 
         if (photonEvent.Code == TRAP_SYNC_PLACE) {
